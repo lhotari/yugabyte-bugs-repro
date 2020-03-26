0785ad71381b7e0781f58c6057a465bfb7b6be9c
Lari Hotari <lari@hotari.net>
2020-03-26 22:41:58 +0200

    Set --ysql_max_read_restart_attempts=128 for YB tserver

---
+++ src/test/java/com/github/lhotari/dbcontainer/yugabyte/readrestart/CustomYugaByteSpringTestContextInitializer.java
@@ -0,0 +1,30 @@
+package com.github.lhotari.dbcontainer.yugabyte.readrestart;
+
+import com.github.lhotari.dbcontainer.DatabaseContainer;
+import com.github.lhotari.dbcontainer.yugabyte.YugaByteDatabaseContainer;
+import com.github.lhotari.spring.dbcontainers.YugaByteSpringTestContextInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomYugaByteSpringTestContextInitializer extends YugaByteSpringTestContextInitializer {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomYugaByteSpringTestContextInitializer.class);
+
+    @Override
+    protected DatabaseContainer createDatabaseContainer(ConfigurableEnvironment environment) {
+        YugaByteDatabaseContainer yugaByteDatabaseContainer = new YugaByteDatabaseContainer() {
+            @Override
+            protected List<String> customizeTserverCommand(List<String> command) {
+                List<String> modifiedCommand = new ArrayList<>(command);
+                modifiedCommand.add("--ysql_max_read_restart_attempts=128");
+                LOG.info("command line: " + modifiedCommand);
+                return modifiedCommand;
+            }
+        };
+        return yugaByteDatabaseContainer;
+    }
+}
+
--- src/test/java/com/github/lhotari/dbcontainer/yugabyte/readrestart/EmbeddedYBReadRestartBugTest.java
+++ src/test/java/com/github/lhotari/dbcontainer/yugabyte/readrestart/EmbeddedYBReadRestartBugTest.java
@@ -1,9 +1,8 @@
 package com.github.lhotari.dbcontainer.yugabyte.readrestart;
 
-import com.github.lhotari.spring.dbcontainers.YugaByteSpringTestContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(initializers = YugaByteSpringTestContextInitializer.class)
+@ContextConfiguration(initializers = CustomYugaByteSpringTestContextInitializer.class)
 public class EmbeddedYBReadRestartBugTest extends AbstractReadRestartBugTest {
 
 }
